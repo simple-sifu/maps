9cc69498c6f85f1df3e6a7e71fbe2e39cf4ca98b
Tommy Han <tommy_han1@intuit.com>
2022-04-08 11:00:36 -0400

    setup google maps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import  { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
+// import  { User } from './User';
+// import { Company } from './Company';
 
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'),{
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
