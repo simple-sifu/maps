b02b90862a51d3c17ee95e536ac9adeadf668fbb
Tommy Han <tommy_han1@intuit.com>
2022-04-09 14:25:58 -0400

    refactor by implementing interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,11 @@
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -30,7 +31,7 @@ export class CustomMap {
 
     marker.addListener('click', () =>{
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there !'
+        content: mappable.markerContent();
       });
       infoWindow.open(this.googleMap, marker);
     });
